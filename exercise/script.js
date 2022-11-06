f94132f89364709d886491d29a23bfca5f172f80
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-06 21:22:17 +0100

    Add files via upload

--- exercise/script.js
+++ exercise/script.js
@@ -1,86 +1,250 @@
 /* 
-🌟 APP: Tip Calculator
+🌟 APP: Fighting Game
 
-These are the 3 functions you must use 👇
-=========================================
-calculateBill()
-increasePeople()
-decreasePeople()
+Create an updateGame() function that will update the DOM with the state of the game 👇
+========================================
+
+- updateGame()
+
+These are the 2 classes you must create and their methods 👇
+========================================
+
+class Player {
+  - strike()
+  - heal()
+}
+
+class Game {
+  - play()
+  - checkIsOver()
+  - declareWinner()
+  - reset()
+}
 
 These functions are hard coded in the HTML. So, you can't change their names.
 
 These are all the DIV ID's you're gonna need access to 👇
 ========================================================
-#1 ID 👉 'billTotalInput' = User input for bill total
-#2 ID 👉 'tipInput' = User input for tip
-#3 ID 👉 'numberOfPeople' = Current number of people you're splitting the bill between
-#4 ID 👉 'perPersonTotal' = Total dollar value owed per person
+#1 ID 👉 'play' = Button to run simulation
+#2 ID 👉 'result' = Div that holds the winner of the match
+#3 ID 👉 'p1Name' = Div that holds player 1's Name
+#4 ID 👉 'p2Name' = Div that holds player 2's Name
+#5 ID 👉 'p1Health' = Div that holds player 1's health
+#6 ID 👉 'p2Health' = Div that holds player 2's health
 */
-const billTotalInput = document.getElementById("billTotalInput")
-const tipInput = document.getElementById("tipInput")
-let numberOfPeople = document.getElementById("numberOfPeople")
-let perPersonTotal = document.getElementById("perPersonTotal")
-// Get global access to all inputs / divs here (you'll need them later 😘)
-// bill input, tip input, number of people div, and per person total div
 
+// ** Grabs elements from the DOM and stores them into variables **
+let playButton = document.getElementById('play')
+let resultDiv = document.getElementById('result')
+let p1NameDiv = document.getElementById('p1Name')
+let p2NameDiv = document.getElementById('p2Name')
+let p1HealthDiv = document.getElementById('p1Health')
+let p2HealthDiv = document.getElementById('p2Health')
 
+// ** Check if either players health is  0 and if it is, then update isOver to true **
+const updateGame = (p1,p2,gameState) => {
+  // Update the DOM with the names and the latest health of players
+  p1NameDiv.innerText = "Bob"
+  p2NameDiv.innerText = "Andre"
+  p1HealthDiv.innerText = p1
+  p2HealthDiv.innerText = p2
 
-// Get number of people from number of people div
-let peopleNumber = Number(numberOfPeople.innerText)
-
-
-// ** Calculate the total bill per person **
-const calculateBill = () => {
-  // get bill from user input & convert it into a number
-  const billTotal = Number(billTotalInput.value)
-  
-  // get the tip from user & convert it into a percentage (divide by 100)
-  const tip = Number(tipInput.value) / 100
-
-  // get the total tip amount
-  const tipAmount = billTotal * tip
-
-  // calculate the total (tip amount + bill)
-  const totalBill = tipAmount + billTotal
-
-  // calculate the per person total (total divided by number of people)
-  const totalAmountPerPerson = totalBill / peopleNumber
-
-  // update the perPersonTotal on DOM & show it to user
-  perPersonTotal.innerText = `$${totalAmountPerPerson.toFixed(2)}`
-
-}
-
-// ** Splits the bill between more people **
-const increasePeople = () => {
-  // increment the amount of people
-  peopleNumber += 1
-
-  // update the DOM with the new number of people
-  numberOfPeople.innerText = peopleNumber
-
-  // calculate the bill based on the new number of people
-  calculateBill()
-
-}
-
-// ** Splits the bill between fewer people **
-const decreasePeople = () => {
-  // guard clause
-  // if amount is 1 or less simply return
-  // (a.k.a you can't decrease the number of people to 0 or negative!)
-  if (peopleNumber > 1) {
-    peopleNumber -= 1
-    numberOfPeople.innerText = peopleNumber
+  // Condition IF either player health is <= 0 then set isOver to true and declareWinner
+  if (p1 <= 0 || p2 <= 0) {
+    isOver = true
   }
-  calculateBill()
-  
-  // decrement the amount of people
-
-
-  // update the DOM with the new number of people
-
-
-  // calculate the bill based on the new number of people
 
 }
+
+// ** Create the Player class which can create a player with all it's attributes and methods **
+// qazi = new Player('Qazi', 100, 7)
+// qazi.name 👉 'Qazi'
+// qazi.health 👉 100
+// qazi.attackDmg 👉 7
+class Player {
+  constructor(name, health, attackDamage) {
+    this.name = name;
+    this.health = health;
+    this.attackDmg = attackDamage;
+  }
+  // ** Attack an enemy with a random number from 0 to YOUR attackDmg bonus **
+  strike (player, enemy, attackDmg) {
+    
+    // Get random number between 1 - 10 and that is damageAmount
+    let damageAmount = Math.floor(Math.random() * 11)
+
+    // Subtract the enemy health with the damageAmount
+    enemy.health -= damageAmount
+
+    //  Update the game and DOM with updateGame()
+    updateGame(this.health, enemy.health, isOver)
+
+    //  Return a message of 'player name attacks enemy name for damageAmount'
+    return `${player.name} attacks ${enemy.name} for ${damageAmount}`
+
+
+  }
+  // ** Heal the player for random number from  1 to 5 **
+  heal (player) {
+    
+    // Get random number between 1 - 5 and store that in hpAmount
+    let hpAmount = Math.floor(Math.random() * 6)
+
+    // Add hpAmount to players health
+    player.health += hpAmount
+
+    //  Update the game and DOM with updateGame()
+    updateGame()
+
+    //  Return a message of 'player name heals for hpAmount HP'
+    return `${player.name} heaals for ${hpAmount} HP`
+
+  }
+}
+
+// ** Create the Game class with all it's attributes and methods to run a match **
+// game = new Game()
+// game.isOver 👉 false
+class Game {
+  constructor() {
+    this.isOver = false;
+  }
+
+  // ** If the game is over (a player has 0 health), declare the winner! **
+  declareWinner(isOver,p1, p2) {
+    
+    // Create a message variable that will hold a message based on the condition
+    let message = ""
+
+    // If isOver is true AND p1 health is <= 0 then update message variable  to 'p1 WINS!'
+    if (isOver && p1.health <= 0) {
+      message = `${p1.name} WINS!` 
+    } else if (isOver && p2.health <= 0) {
+      message = `${p2.name} WINS!`
+    }
+    let victorySound = new Audio('./sounds/victory.mp3')
+    victorySound.play()
+
+    return message
+    // Else if isOver is true AND p2 health is <= 0 then update message variable  to 'p2 WINS!'
+    // Play victory sound
+
+    // Return message variable 
+
+  }
+
+  // ** Reset the players health back to it's original state and isOver to FALSE **
+  reset(p1,p2) {
+    p1.health = 100
+    p2.health = 100
+    this.isOver = false
+    resultDiv.innerText = ""
+    updateGame(p1, p1, this.isOver)
+    // set p1 health and p2 health back to 100 and isOver back to false and clear resultDiv.innerText and don't forget to updateGame()
+
+  }
+  
+  // ** Simulates the whole match untill one player runs out of health **
+  play(p1, p2) {
+    this.reset(p1, p2)
+    // Reset to make sure player health is back to full before starting
+
+    // Make sure the players take turns untill isOver is TRUE
+    while (!this.isOver) {
+      //Make sure both players get strike() and heal() once each loop
+      Player.strike(p1, p2, 10)
+      Player.heal(p1)
+      updateGame(p1, p2, this.isOver)
+      
+      Player.strike(p2, p1, 10)
+      Player.heal(p2)
+      updateGame(p1, p2, this.isOver)
+    }
+    this.declareWinner(this.isOver, p1, p2)
+    // Once isOver is TRUE run the declareWinner() method 
+
+  }
+
+}
+
+// ** Create 2 players using the player class **
+const player1 = new Player("Bob", 100, 5)
+const player2 = new Player("Andre", 100, 5)
+
+
+// ** Save original Player Data into a variable in order to reset **
+
+let p1 = player1;
+let p2 = player2;
+
+// ** Create the game object from the Game class **
+const game = new Game()
+
+// ** Save intial isOver from the game object inside this variable **
+let gameState = game.isOver
+
+updateGame(p1, p2, gameState)
+
+// ** Intialize the game by calling updateGame() **
+
+
+// ** Add a click listener to the simulate button that runs the play() method on click and pass in the players **
+
+
+// Add functionality where players can press a button to attack OR heal
+
+// ** Player 1 Controls **
+document.addEventListener('keydown', function(e) {
+  // if you press Q AND the enemy health is greater than 0 AND isOver is still false then strike()
+  if (e.key == 'q' || e.key == 'Q' && p1.health > 0 && !isOver) {
+    player1.strike(p1, p2, player1.attackDmg)
+    let attackSound = new Audio('./sounds/fastpunch.mp3')
+    attackSound.play()
+  } 
+
+    // After striking then play attack sound
+
+});
+
+document.addEventListener('keydown', function(e) {
+  
+  // if you press a AND the player health is greater than 0 AND isOver is still false then strike()
+  if (e.key == 'a' || e.key == 'A' && p1.health > 0 && !isOver) {
+    player1.heal(p1)
+    let healSound = new Audio('./sounds/fastheal.mp3')
+    healSound.play()
+  } 
+
+    // After healing then play heal sound
+
+});
+
+// ** Player 2 Controls **
+document.addEventListener('keydown', function(e) {
+  
+  // if you press p AND enemy health is greater than 0 AND isOver is still false then stike()
+  if (e.key == 'p' || e.key == 'P' && p2.health > 0 && !isOver) {
+    player2.strike(p2, p1, player2.attackDmg)
+    let attackSound = new Audio('./sounds/quickhit.mp3')
+    attackSound.play()
+  } 
+
+    // After striking then play attack sound
+
+});
+
+document.addEventListener('keydown', function(e) {
+  // if you press l AND the player health is greater than 0 AND isOver is still false then heal()
+  if (e.key == 'l' || e.key == 'L' && p2.health > 0 && !isOver) {
+    player2.heal(p2)
+    let healSound = new Audio('./sounds/quickheal.mp3')
+    healSound.play()
+  } 
+
+    // After healing then play heal sound
+
+});
+
+
+
+
