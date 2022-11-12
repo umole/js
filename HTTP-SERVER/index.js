2e14870efffecb5e8f7b3821546bb3497f0b13d5
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-12 19:46:11 +0100

    Add files via upload

---
+++ HTTP-SERVER/index.js
@@ -0,0 +1,39 @@
+const http = require('http')
+
+const PORT = 3000;
+
+const server = http.createServer();
+server.on('request', (req, res) => {
+    if (req.url === '/friends') {
+        // res.writeHead(200, {
+        //     'content-Type': 'application/json' 
+        // });
+        res.statusCode = 200;
+        res.setHeader('content-Type', 'application/json')
+        res.end(JSON.stringify({
+            name: "Andrew Umole",
+            age: 23,
+            skill: "Progrmming",
+            specialisation: "Backend",
+            language: "JavaScript"
+        }));
+    } else if (req.url === '/messages') {
+        res.setHeader('content-Type', 'text/html');
+        res.write('<html>');
+        res.write('<body>');
+        res.write('<ul>');
+        res.write('<li>Hello Andre!</li>');
+        res.write('<li>What are your toughts on cryptocurrency?</li>');
+        res.write('</ul>');
+        res.write('</body>');
+        res.write('</html>');
+        res.end();
+    } else {
+        res.statusCode = 404;
+        res.end()
+    }
+})
+
+server.listen(PORT, () => {
+    console.log(`Listening on port ${PORT}...`)
+}); //127.0.0.1 => localhost
