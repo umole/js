65df5b065babc809cc341416b02a796db2e6d0ca
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-14 22:20:22 +0100

    Add files via upload

---
+++ routes/messages.router.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const messageController = require('../controllers/messages.controller');
+
+
+const messagesRouter = express.Router();
+
+messagesRouter.get('/', messageController.getMessage);
+messagesRouter.post('/', messageController.postMessage);
+
+module.exports = messagesRouter;
