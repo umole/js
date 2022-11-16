e03b9b0f5194a74cdeaaab02739a45cdf7fbb2d5
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-16 19:21:58 +0100

    Add files via upload

---
+++ src/server.js
@@ -0,0 +1,23 @@
+const http = require('http');
+
+const app = require('./app');
+
+const PORT = process.env.PORT || 8000;
+
+const { loadPlanetsData } = require('./models/planets.model');
+
+const server = http.createServer(app);
+
+const startFunction = async () => {
+    await loadPlanetsData();
+
+    server.listen(PORT, () => {
+    console.log(`Listening on PORT: ${PORT}...`);
+});
+};
+
+startFunction();
+
+
+
+
