99c806313345a08e219d85f224d5b3dd755e7549
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-19 17:40:30 +0100

    Add files via upload

---
+++ src/routes/launches/launches.test.js
@@ -0,0 +1,14 @@
+describe('TEST GET /launches', () => {
+    test('It should respond with 200 success', () => {
+        const response = 200;
+        expect(response).toBe(200)
+    })
+})
+
+describe('TEST POST /launch', () => {
+    test('It should respond with 200 success', () => {
+
+    });
+    test('It should catch missing required properties', () => {});
+    test('It should catch invalid dates', () => {});
+})
