f262ea4c755c34cc4f703a68cb3592897b4cfd93
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-20 03:00:26 +0100

    Add files via upload

--- src/routes/launches/launches.test.js
+++ src/routes/launches/launches.test.js
@@ -1,13 +1,28 @@
+const request = require('supertest');
+const app = require('../../app');
+
+
 describe('TEST GET /launches', () => {
-    test('It should respond with 200 success', () => {
-        const response = 200;
-        expect(response).toBe(200)
+    test('It should respond with 200 success', async () => {
+        const response = await request(app)
+        .get('/launches')
+        .expect('Content-Type', /json/)
+        .expect(200);
     })
 })
 
 describe('TEST POST /launch', () => {
-    test('It should respond with 200 success', () => {
-
+    test('It should respond with 201 created', async () => {
+        const response = await request(app)
+        .post('/launches')
+        .send({
+            mission: "ZTM155",
+            rocket: "ZTM Expeimental ISL",
+            target: "Kepler-186 f",
+            launchDate: "July 23, 2023"
+        })
+        .expect('Content-Type', /json/)
+        .expect(201);
     });
     test('It should catch missing required properties', () => {});
     test('It should catch invalid dates', () => {});
