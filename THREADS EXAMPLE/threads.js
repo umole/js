a46d6dc7d0b994271a595c9849122ecdf232ce40
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-24 20:30:32 +0100

    Add files via upload

--- THREADS EXAMPLE/threads.js
+++ THREADS EXAMPLE/threads.js
@@ -13,6 +13,12 @@ const {
     new Worker(__filename, {
         workerData: [9, 1, 2, 0]
       });
+    new Worker(__filename, {
+        workerData: [8, 1, 3, 0]
+      });
+    new Worker(__filename, {
+        workerData: [18, 13, 3, 10]
+      });
   } else {
     console.log(`Worker! process ID: ${process.pid}`);
     console.log(`${workerData} sorted is ${workerData.sort()}`);
