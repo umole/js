f905511d342fe33755fc362a89217512fb6507c8
Umole Andrew <39883080+umole@users.noreply.github.com>
2022-11-27 22:16:07 +0100

    Add files via upload

--- src/server.js
+++ src/server.js
@@ -4,6 +4,8 @@ const app = require('./app');
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = ""
+
 const { loadPlanetsData } = require('./models/planets.model');
 
 const server = http.createServer(app);
