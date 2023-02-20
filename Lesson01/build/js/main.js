dd52dc5b3f41fd6793851f4c000ac82ff9555a30
Umole Andrew <39883080+umole@users.noreply.github.com>
2023-02-20 08:39:19 +0100

    Add files via upload

---
+++ Lesson01/build/js/main.js
@@ -0,0 +1,4 @@
+"use strict";
+let strArray = ['one', 'Bisi', 'Money'];
+let guitars = ['Power', 'St. Patrick', 2019];
+let mixedData = ['Lexus', 2022, true];
---
+++ Lesson01/src/main.ts
@@ -0,0 +1,5 @@
+let strArray = ['one', 'Bisi', 'Money']
+
+let guitars = ['Power', 'St. Patrick', 2019]
+
+let mixedData = ['Lexus', 2022, true]
